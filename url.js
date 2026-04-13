86001599442cf7db96cb334438533fe489a4cfbf
kel44885-ai <kel44885@gmail.com>
2026-04-13 12:03:15 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,8 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://forms.gle/27xnYXAM4RzLuoch6',
+	'https://forms.gle/bhP8wmWp6mKDKzGw9',
+	'https://forms.gle/XtSVTTvBEMe5AXB37',
+	'https://forms.gle/kCnByNTFboSihKhy7',
 ];
